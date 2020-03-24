d76c29915e908fa3a8806fbf2a669236df19e186
Evan Cameron <cameron.evan@gmail.com>
2020-03-24 11:18:43 -0400

    zip uses queued items

--- src/par_stream/zip.rs
+++ src/par_stream/zip.rs
@@ -16,35 +16,74 @@ pin_project! {
     /// [`zip`]: trait.ParallelStream.html#method.zip
     /// [`ParallelStream`]: trait.ParallelStream.html
     #[derive(Clone, Debug)]
-    pub struct Zip<A, B> {
+    pub struct Zip<A, B>
+    where
+        A: ParallelStream,
+        B: ParallelStream,
+    {
         #[pin]
         stream: A,
         #[pin]
         other: B,
         limit: Option<usize>,
+        queued1: Option<A::Item>,
+        queued2: Option<B::Item>,
     }
 }
 
-impl<A: ParallelStream, B: ParallelStream> Zip<A, B> {
+impl<A, B> Zip<A, B>
+where
+    A: ParallelStream,
+    B: ParallelStream,
+{
     pub(super) fn new(stream: A, other: B) -> Self {
         Self {
             limit: cmp::min(stream.get_limit(), other.get_limit()),
             other,
             stream,
+            queued1: None,
+            queued2: None,
         }
     }
 }
 
-impl<A: ParallelStream, B: ParallelStream> ParallelStream for Zip<A, B> {
+impl<A, B> ParallelStream for Zip<A, B>
+where
+    A: ParallelStream,
+    B: ParallelStream,
+    A::Item: Sync,
+    B::Item: Sync,
+{
     type Item = (A::Item, B::Item);
 
     fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
         let this = self.project();
-        let next = ready!(this.stream.poll_next(cx));
-        let other_next = ready!(this.other.poll_next(cx));
-        match (next, other_next) {
-            (Some(a), Some(b)) => Poll::Ready(Some((a, b))),
-            _ => Poll::Ready(None),
+        let mut stream_done = false;
+        let mut other_done = false;
+        if this.queued1.is_none() {
+            match this.stream.poll_next(cx) {
+                Poll::Ready(Some(item1)) => *this.queued1 = Some(item1),
+                Poll::Ready(None) | Poll::Pending => {
+                    stream_done = true;
+                }
+            }
+        }
+        if this.queued2.is_none() {
+            match this.other.poll_next(cx) {
+                Poll::Ready(Some(item2)) => *this.queued2 = Some(item2),
+                Poll::Ready(None) | Poll::Pending => {
+                    other_done = true;
+                }
+            }
+        }
+
+        if this.queued1.is_some() && this.queued2.is_some() {
+            let pair = (this.queued1.take().unwrap(), this.queued2.take().unwrap());
+            Poll::Ready(Some(pair))
+        } else if stream_done && other_done {
+            Poll::Ready(None)
+        } else {
+            Poll::Pending
         }
     }
 
